9e539a145c79b27b8a29e5d05a055913d8b3a962
javsezlol1 <95410843+javsezlol1@users.noreply.github.com>
2023-10-03 21:42:24 +0800

    fix
    replace js incase of update

--- install.py
+++ install.py
@@ -6,7 +6,5 @@ import shutil
 
 js_file = os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), "scripts"), "Stylez.js")
 os_js_folder = os.path.join(os.getcwd(), "javascript")
-
-if not os.path.exists(os.path.join(os_js_folder, "Stylez.js")):
-    shutil.copy(js_file, os_js_folder)
+shutil.copy(js_file, os_js_folder)
 
